d321e6289866d8d99c267804fdbe7fe0d3e4254f
khoavo1901 <vo.kh@northeastern.edu>
2022-04-11 17:20:30 -0400

    My commit message

--- js/map.js
+++ js/map.js
@@ -7,72 +7,27 @@ let osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 //initialize svg to add to map
 L.svg({clickable:true}).addTo(map)
 
-const overlay = d3.select(map.getPanes().overlayPane)
-const svg = overlay.select('svg').attr("pointer-events", "auto")
+circlesLayer = L.layerGroup().addTo(map);
+circlesLayer.clearLayers()
+
+
+d3.csv("data/finalmapdata.csv").then(function(data){
+  data.forEach(function(d){
+  let circle = L.circle([d.lat, d.long], 7000, {
+        color: '#fd8d3c',
+        weight: 2,
+        fillColor: '#fecc5c',
+          fillOpacity: 0.5
+          })
+    let txt = "Facility Name: "+d.FacilityName 
+    +"<br>City: "+ d.City
+    +"<br> Require paperback book: " + d.only_paperback
+    +"<br> Good with used book: " + d.condition_good_used_okay
+    +"<br> Require bundle: " + d.bundle
+    +"<br> Good with hardcover book: " + d.hardcover_okay
+    circle.bindPopup(txt)
+    circlesLayer.addLayer(circle) })});
+
   
-d3.csv('data/data.csv').then(function(data){
-  let cities = svg.selectAll('circle')
-                  .attr("class", "Dots")
-                  .data(data)
-                  .enter().append('circle')
-                      .attr("id", "dotties")
-                      .attr("fill", "steelblue")
-                      .attr("stroke", "black")
-                      .attr("cx", d =>
-                       map.latLngToLayerPoint([d.latitude, d.longitude]).x)
-                      .attr("cy", d =>
-                       map.latLngToLayerPoint([d.latitude, d.longitude]).y)
-                      .attr("r",5)
-                      .on('mouseover', function() { //function to add mouseover event
-                        d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
-                          .duration('150') //how long we are transitioning between the two states (works like keyframes)
-                          .attr("fill", "red") //change the fill
-                          .attr('r', 10) //change radius
-                      })
-                      .on('mouseout', function() { //reverse the action based on when we mouse off the the circle
-                        d3.select(this).transition()
-                          .duration('150')
-                          .attr("fill", "steelblue")
-                          .attr('r', 5)
-                      });
-    
 
-
-// create custom dispatch events
-// var dispatch = d3.dispatch("mouseover", "mouseout");
-
-// // create a listener function that change the color of the circle to red upon
-// // the yellow square being clicked
-// dispatch.on("mouseover", function() {
-//   d3.select(this).transition()
-//   .duration('150') //how long we are transitioning between the two states (works like keyframes)
-//   .attr("fill", "red") //change the fill
-//   .attr('r', 10) //change radius
-// });
-
-// // listener function that change the color of the rectangle to green
-// // if red circle is double-clicked
-// dispatch.on("mouseout", function() {
-//   d3.select(this).transition()
-//   .duration('150')
-//   .attr("fill", "steelblue")
-//   .attr('r', 5)
-// });
-
-
-// // listener event for rectangle
-// cities.on('mouseover', function(){
-//   dispatch.call("mouseover")
-// });
-
-// cities.on('mouseout', function(){
-//   dispatch.call("mouseout")
-// })
-
-const update = () => cities
-  .attr("cx", d => map.latLngToLayerPoint([d.latitude, d.longitude]).x)
-  .attr("cy", d => map.latLngToLayerPoint([d.latitude, d.longitude]).y) 
-
-map.on("zoomend", update)
-});
 
