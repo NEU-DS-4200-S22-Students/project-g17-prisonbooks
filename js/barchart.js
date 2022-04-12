aa408a73fb9fd3050a665b6791492e9b31e681ab
khoavo1901 <vo.kh@northeastern.edu>
2022-04-11 22:35:54 -0400

    Add bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -1,124 +1,123 @@
-// Probably a better idea is to read this in
-let data = [
-  {donor:"Kieran Mcgrath Household",count:215},
-  {donor:"Chad Mohler Household",count:196},
-  {donor:"Anonymous Household",count:119},
-  {donor:"Anonymous Anonymous Household",count:107},
-  {donor:"Margaret Witham Household",count:103},
-  {donor:"Susan D Foster Household",count:82},
-  {donor:"Lori Shemanski Household",count:77},
-  {donor:"Jennifer Hartwell Household",count:73},
-  {donor:"Ann Crowley Household",count:71},
-  {donor:"Arthur Clark Household",count:68},
-  {donor:"Mark Duske Household",count:65},
-  {donor:"Anna Rothman Household",count:62},
-  {donor:"Network Good Household",count:62},
-  {donor:"Vicki Coffman Household",count:54},
-  {donor:"Katryna Hadley Household",count:52}
-];
+//Define data
 
-// Create SVG
-let
-  width = 800,
-  height = 600;
-  
-let margin = {
-  top: 40,
-  bottom: 30,
-  left: 30,
-  right: 30
-};
+d3.json('data/topdonors.json').then(function(data) {
+   
 
-let svg = d3.select('#vis3')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height + 140)
-.style('background', '#c5e1e6');
-
-// Define Scales
-let yScale = d3.scaleLinear()
-  .domain([0, 250]) // make this dynamic
-  .range([height - margin.bottom, margin.top]);
-
-let xScale = d3.scaleBand()
-  .domain(
-    data.map(function(d) {
-      return d.donor;
-    })
-  )
-  .range([margin.left, width - margin.right - 10])
-  .padding(0.35);
-
-//Draw Axes
-let yAxis = svg
-  .append('g')
-    .attr('transform', `translate(${margin.left},0)`)
-    .call(d3.axisLeft().scale(yScale));
-
-//Add label
-yAxis
-  .append('text')
-    .attr('y', 30)
-    .attr('x', 20)
-    .style('stroke', 'black')
-    .text('Count');
-
-let xAxis = svg
-  .append('g')
-    .attr('transform', `translate(0,${height - margin.bottom})`)
-    //.attr("transform", "rotate(0)")
-    .call(d3.axisBottom().scale(xScale));
-
-    // Trying to add some rotation to the x-labels
+    // Create SVG
+    let
+      width = 800,
+      height = 600;
+     
+    let margin = {
+      top: 40,
+      bottom: 30,
+      left: 30,
+      right: 30
+    };
+    
+    let svg = d3
+      .select('#vis2')
+      .append('svg')
+        .attr('width', width)
+        .attr('height', height + 140)
+        .style('background', '#c5e1e6');
+    
+    // Define Scales
+    let yScale = d3.scaleLinear()
+      .domain([0, 250]) // make this dynamic
+      .range([height - margin.bottom, margin.top]);
+    
+    let xScale = d3.scaleBand()
+      .domain(
+        data.map(function(d) {
+          return d.donor;
+        })
+      )
+      .range([margin.left, width - margin.right - 10])
+      .padding(0.35);
+    
+    //Draw Axes
+    let yAxis = svg
+      .append('g')
+        .attr('transform', `translate(${margin.left},0)`)
+        .call(d3.axisLeft().scale(yScale));
+    
+    //Add label
+    yAxis
+      .append('text')
+        .attr('y', 30)
+        .attr('x', 20)
+        .style('stroke', 'black')
+        .text('Count');
+    
+    let xAxis = svg
+      .append('g')
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        //.attr("transform", "rotate(0)")
+        .call(d3.axisBottom().scale(xScale));
+    
+        // Trying to add some rotation to the x-labels
+        xAxis
+          .selectAll("text")  
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-65)");
+     
+    //Add label
     xAxis
-      .selectAll("text")  
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-65)");
-  
-//Add label
-xAxis
-  .append('text')
-    .attr('x', width - margin.left + 5)
-    .attr('y', -10)
-    .style('stroke', 'black')
-    .text('Donor');
+      .append('text')
+        .attr('x', width - margin.left + 5)
+        .attr('y', -10)
+        .style('stroke', 'black')
+        .text('Donor');
+    
+    
+          // ----------------
+      // Create a tooltip
+      // ----------------
 
-//Draw bars
-let bar = svg
-  .selectAll('rect')
-    .data(data)
-  .enter()
-  .append('rect')
-    .attr('x', function(d) {
-      return xScale(d.donor);
-    })
-    .attr('y', function(d) {
-      return yScale(d.count);
-    })
-    .attr('width', xScale.bandwidth())
-    .attr('fill', 'steelblue')
-    .attr('height', function(d) {
-      return height - margin.bottom - yScale(d.count);
-    });
+    // Three function that change the tooltip when user hover / move / leave a cell
+var div = d3.select("#vis2").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-//Interaction
-bar
-    .on('mouseover', function(d) {
-      d3.select(this)
-        .transition()
-          .delay(2)
-          .duration(1000)
-        .style('fill', '#69b3a2')
-        .attr('opacity', '0.80');
-    })
+var mouseover = function(event, d) {
+    div.transition()
+        .duration(200)
+        .style("opacity", .9);
+    div.html("Count: " + d.count)
+        .style("left", (event.pageX) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    };
 
-    .on('mouseout', function(d) {
-      d3.select(this)
-        .transition()
-          .delay(2)
-          .duration(1000)
-        .style('fill', 'steelblue')
-        .attr('opacity', '0.9');
-    });
+
+var mouseout = function(event, d) {
+    div.transition()
+        .duration(500)
+        .style("opacity", 0);
+    };
+
+    
+    //Draw bars
+    let bar = svg
+      .selectAll('rect')
+        .data(data)
+      .enter()
+      .append('rect')
+        .attr('x', function(d) {
+          return xScale(d.donor);
+        })
+        .attr('y', function(d) {
+          return yScale(d.count);
+        })
+        .attr('width', xScale.bandwidth())
+        .attr('fill', 'steelblue')
+        .attr('height', function(d) {
+          return height - margin.bottom - yScale(d.count);
+        })
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout)
+    
+      });
+    
